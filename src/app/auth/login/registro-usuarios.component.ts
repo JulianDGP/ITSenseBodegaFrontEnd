9c177bcf2608802fa49f903e304b01c65b6c0217
JulianDGP <juliandgp@hotmail.com>
2023-01-30 11:44:11 -0500

    se sube el componente crear usuario

--- src/app/auth/login/registro-usuarios.component.ts
+++ src/app/auth/login/registro-usuarios.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { NuevoUsuario } from 'src/app/models/nuevo-usuario';
+import { AuthService } from 'src/app/service/auth.service';
+import { TokenService } from 'src/app/service/token.service';
 
 
 @Component({
@@ -7,5 +12,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-usuarios.component.css']
 })
 export class RegistroUsuariosComponent {
+  nuevoUsuario!: NuevoUsuario;
+  nombre!: string;
+  nombreUsuario!: string;
+  email!: string;
+  password!: string;
+  errMsj!: string;
+  isLogged = false;
 
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    }
+  }
+
+  onRegister(): void {
+    this.nuevoUsuario = new NuevoUsuario(this.nombre, this.nombreUsuario, this.email, this.password);
+    this.authService.nuevoUsuario(this.nuevoUsuario).subscribe(
+      data => {
+        this.toastr.success('Cuenta Creada', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+
+        this.router.navigate(['/login']);
+      },
+      err => {
+        this.errMsj = err.error.mensaje;
+        this.toastr.error(this.errMsj, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        // console.log(err.error.message);
+      }
+    );
+  }
 }
